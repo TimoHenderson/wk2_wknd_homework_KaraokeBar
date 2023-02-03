53091a6207329bfee01ec9b257c1ba794c6690d2
TimoHenderson <tjhenderson2@gmail.com>
2023-02-03 22:18:16 +0000

    Added Song.artist

--- tests/song_test.py
+++ tests/song_test.py
@@ -4,9 +4,14 @@ from src.song import Song
 
 class TestSong(unittest.TestCase):
     def setUp(self):
-        self.song = Song("Walk")
+        self.song = Song("Walk", "Pantera")
 
     def test_has_name(self):
         actual = self.song.name
         expected = "Walk"
+        self.assertEqual(actual, expected)
+
+    def test_has_artist(self):
+        actual = self.song.artist
+        expected = "Pantera"
         self.assertEqual(actual, expected)
